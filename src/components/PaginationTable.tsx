4d7503871c6a550627ab43d78f52757ace9ab358
Raul Noguera Macbook pro <raul.noguera.rocha@gmail.com>
2026-06-09 19:24:30 -0400

    fix: change button state from disabled to hidden in PaginationTable component

--- src/components/PaginationTable.tsx
+++ src/components/PaginationTable.tsx
@@ -21,7 +21,7 @@ export const PaginationTable = ({
       <div className="d-flex align-items-center" style={{ gap: "0.3rem" }}>
         <button
           className="btn btn-sm btn-primary"
-          disabled={page <= 1}
+          hidden={page <= 1}
           onClick={() => onPageChange(Math.max(1, page - 1))}
         >
           <i className="fa fa-chevron-left"></i> Anterior
@@ -37,7 +37,7 @@ export const PaginationTable = ({
         ))}
         <button
           className="btn btn-sm btn-primary"
-          disabled={page >= totalPages}
+          hidden={page >= totalPages}
           onClick={() => onPageChange(Math.min(totalPages, page + 1))}
         >
           Proximo <i className="fa fa-chevron-right"></i>
