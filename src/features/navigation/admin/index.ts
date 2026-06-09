eed24fdcb67eb9ca9b17e9be36fd4141be7cc60b
Raul Noguera Macbook pro <raul.noguera.rocha@gmail.com>
2026-06-09 11:59:05 -0400

    refactor: update navigation menus by removing unused items and reorganizing structure

--- src/features/navigation/admin/index.ts
+++ src/features/navigation/admin/index.ts
@@ -7,28 +7,4 @@ export const adminMenu: MenuItem[] = [
     icon: "th-large",
     name: "Dashboard",
   },
-  {
-    id: "coaches",
-    route: "/coaches",
-    icon: "fa fa-person",
-    name: "Entrenadores",
-  },
-  {
-    id: "athletes",
-    route: "/athletes",
-    icon: "fa fa-running",
-    name: "Atletas",
-  },
-  {
-    id: "parents",
-    route: "/parents",
-    icon: "fa fa-users",
-    name: "Tutores",
-  },
-  {
-    id: "assignments",
-    route: "/assignments",
-    icon: "fa fa-tasks",
-    name: "Asignaciones",
-  },
 ];
--- src/features/navigation/superadmin/index.ts
+++ src/features/navigation/superadmin/index.ts
@@ -8,39 +8,15 @@ export const superAdminMenu: MenuItem[] = [
     name: "Dashboard",
   },
   {
-    id: "admins",
-    route: "/admins",
-    icon: "users",
-    name: "Administradores",
-  },
-  {
-    id: "coaches",
-    route: "/coaches",
-    icon: "fa fa-person",
-    name: "Entrenadores",
-  },
-  {
-    id: "athletes",
-    route: "/athletes",
-    icon: "fa fa-running",
-    name: "Atletas",
-  },
-  {
-    id: "parents",
-    route: "/parents",
-    icon: "fa fa-users",
-    name: "Tutores",
-  },
-  {
     id: "assignments",
     route: "/assignments",
     icon: "fa fa-tasks",
     name: "Asignaciones",
   },
   {
-    id: "reports",
-    route: "/reports",
-    icon: "fa fa-chart-bar",
-    name: "Reportes",
+    id: "admins",
+    route: "/admins",
+    icon: "users",
+    name: "Administradores",
   },
 ];
