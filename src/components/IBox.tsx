6e1acc98e5ac06783679c4ea16bbea74e0550f77
Raul Noguera Macbook pro <raul.noguera.rocha@gmail.com>
2026-06-20 13:58:48 -0400

    feat: updating Ibox component adding collapse or uncollapse action

--- src/components/IBox.tsx
+++ src/components/IBox.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 interface IBoxProps {
   title: string;
   children: React.ReactNode;
 }
 
 export const IBox = (props: IBoxProps) => {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <div className="ibox">
+    <div className={`ibox ${collapsed ? "collapsed" : ""}`}>
       <div className="ibox-title">
         <h5>{props.title}</h5>
         <div className="ibox-tools">
-          <a className="collapse-link">
+          <a className="collapse-link" onClick={() => setCollapsed(!collapsed)}>
             <i className="fa fa-chevron-up"></i>
           </a>
         </div>
